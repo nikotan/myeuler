ce5fa8c70c59c559546d510c14ebd7bb6bf4995d
Yusuke Konishi <niko25pu@gmail.com>
2011-05-25 01:14:54 +0900

    Add Solver017-019

---
+++ test/euler/Solver019Test.java
@@ -0,0 +1,30 @@
+package euler;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Solver019Test
+{
+
+  @Before
+  public void setUp() throws Exception
+  {
+  }
+
+  @After
+  public void tearDown() throws Exception
+  {
+  }
+
+  @Test
+  public void testGetDays()
+  {
+    Solver019 solver = new Solver019();
+    assertEquals(366, solver.getDays(1901, 1, 1));
+    assertEquals(397, solver.getDays(1901, 2, 1));
+  }
+
+}
